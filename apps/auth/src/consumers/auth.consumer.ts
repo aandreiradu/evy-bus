0fe2f8f5af083fabf28b6780ddf72358a1079622
Andrei Radu <raduandrei697@gmail.com>
2024-04-17 00:43:35 +0300

    auth consumer

--- apps/auth/src/consumers/auth.consumer.ts
+++ apps/auth/src/consumers/auth.consumer.ts
@@ -3,13 +3,13 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
-  OnApplicationBootstrap,
+  OnApplicationShutdown,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SQS } from 'aws-sdk';
 
 @Injectable()
-export class AuthEventConsumer implements OnApplicationBootstrap {
+export class AuthEventConsumer implements OnApplicationShutdown {
   private readonly logger: Logger = new Logger(AuthEventConsumer.name);
   #activeConsumer = true;
 
@@ -24,11 +24,16 @@ export class AuthEventConsumer implements OnApplicationBootstrap {
     for (let i = 1; i <= eventConsumerInstances; i++) {
       this.createSqsListener(i);
     }
-
-    console.log('created consumers');
   }
 
-  onApplicationBootstrap() {
+  beforeApplicationShutdown() {
+    this.logger.warn(
+      '== Closing auth consumer connections (shutdown signal) ==',
+    );
+    this.#activeConsumer = false;
+  }
+
+  onApplicationShutdown(signal: string) {
     this.logger.warn(`Closing the Auth consumer...`);
 
     // set a timeout to complete running actions and force shutdown
@@ -43,8 +48,8 @@ export class AuthEventConsumer implements OnApplicationBootstrap {
   async createSqsListener(instanceNo: number) {
     const config: SQS.ReceiveMessageRequest = {
       QueueUrl: this.configService.get('AWS_SQS_AUTH_QUEUE_URL'),
-      MaxNumberOfMessages: 1, // process 1 message per consumer,
-      WaitTimeSeconds: 20, // second to retry fetching new event
+      MaxNumberOfMessages: 5, // process 1 message per consumer,
+      WaitTimeSeconds: 5, // second to retry fetching new event
       VisibilityTimeout: 120, // seconds before the event is available again for the next consumer,
       MessageAttributeNames: ['All'],
       AttributeNames: ['ApproximateReceiveCount'],
@@ -68,7 +73,7 @@ export class AuthEventConsumer implements OnApplicationBootstrap {
         '1';
     } catch (error) {
       this.logger.warn(
-        `Could not connect awb consumer instance ${instanceNo} to queue ${this.configService.get(
+        `Could not connect auth consumer instance ${instanceNo} to queue ${this.configService.get(
           'AWS_SQS_AUTH_QUEUE_URL',
         )}`,
         { name: AuthEventConsumer.name },
@@ -77,17 +82,34 @@ export class AuthEventConsumer implements OnApplicationBootstrap {
       throw new Error(error);
     }
 
-    // while (this.#activeConsumer) {
-    //   try {
-    //     const data = await sqsConumser.receiveMessage(config).promise();
-    //     console.log('data', data);
-    //     return true;
-    //   } catch (error) {
-    //     this.logger.error('Failed to consumer messages');
-    //     this.logger.error(JSON.stringify(error));
+    while (this.#activeConsumer) {
+      console.log('while loop');
+      try {
+        console.log('instanceNo polling from queue', instanceNo);
+        const data = await sqsConumser.receiveMessage(config).promise();
+        console.log('data', data);
 
-    //     throw new InternalServerErrorException();
-    //   }
-    // }
+        if (data?.Messages[0]?.ReceiptHandle) {
+          console.log(`InstanceNo ${instanceNo} found a message`);
+          await this.sqsService.removeEventsFromQueue(
+            this.configService.get('AWS_SQS_AUTH_QUEUE_URL'),
+            sqsConumser,
+            data?.Messages[0]?.ReceiptHandle,
+          );
+
+          console.log(
+            `instanceNo ${instanceNo} successfully removed from queue`,
+          );
+        }
+      } catch (error) {
+        this.logger.error('Failed to consumer messages');
+        this.logger.error(JSON.stringify(error));
+
+        console.log('closing the while loop');
+        this.#activeConsumer = false;
+
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
